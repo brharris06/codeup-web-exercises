758788612afcbceab4e0e3433049e4b6d14bc204
Brandon Harris <brandon.r.harris06@gmail.com>
2022-02-07 13:52:46 -0600

    Adding Morning Function Exercises

---
+++ js/js-warmups.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Write a function that takes in an input and returns the string "I have no idea what you just said";
+
+function stringExample(string){
+    return "I have no idea what you just said";
+}
+
+console.log(stringExample("string"));
+
+
+//write a function that takes in a number and returns that number times 2
+//so if you give it 2 it returns 4
+//if you give it 100 it returns 200
+
+function numberExample(x){
+    return x * 2
+}
+
+console.log(numberExample(5));
+
