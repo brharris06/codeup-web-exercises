7526eb33bef2a187bd80257da90abf1bae84be1d
Brandon Harris <brandon.r.harris06@gmail.com>
2022-02-07 15:48:43 -0600

    Adding Do While Loops Exercise

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -1,31 +1,39 @@
-// var slices = 4;
-// while(slices > 0) {
-//     console.log("I'll have a slice!");
-//     slices = slices - 1;
-//     console.log("Now there's " + slices + " left");
+// // var slices = 4;
+// // while(slices > 0) {
+// //     console.log("I'll have a slice!");
+// //     slices = slices - 1;
+// //     console.log("Now there's " + slices + " left");
+// // }
+// // console.log("Awwwwwww........ no more pizza :( ");
+//
+//
+// // var counter = 0;
+// // while(counter <= 100){
+// //     console.log(counter);
+// //     // counter = counter + 1;
+// //     counter ++;
+// // }
+//
+// // var counter = 100;
+// // while(counter >= 0){
+// //     console.log(counter);
+// //     counter --;
+// // }
+//
+// var counter = 1;
+// var number = 5;
+// while (counter <=5){
+//     number = number * 2;
+//     console.log(number);
+//     counter = counter + 1;
 // }
-// console.log("Awwwwwww........ no more pizza :( ");
 
+// Do-while
+var number = Math.floor(Math.random() *6) + 1;
+var guess;
 
-// var counter = 0;
-// while(counter <= 100){
-//     console.log(counter);
-//     // counter = counter + 1;
-//     counter ++;
-// }
+do {
+    guess = parseInt(prompt("Enter a number between 1 and 6"));
+} while (guess !== number);
 
-// var counter = 100;
-// while(counter >= 0){
-//     console.log(counter);
-//     counter --;
-// }
-
-var counter = 1;
-var number = 5;
-while (counter <=5){
-    number = number * 2;
-    console.log(number);
-    counter = counter + 1;
-}
-
-
+alert("Your guess of " + guess + " matches the number " + number + "!");
--- js/while.js
+++ js/while.js
@@ -1,8 +1,46 @@
-"use strict";
+// "use strict";
+//
+// var number = 2;
+// while(number <= 65536){
+//     console.log(number);
+//     number = number * 2;
+// }
 
-var number = 2;
-while(number <= 65536){
-    console.log(number);
-    number = number * 2;
-}
+// var amountofCones = Math.floor(Math.random() * 100) + 50;
+// var Conesbought;
+//
+// do {
+//     Conesbought = Math.floor(Math.random() * 5) + 1;
+//     amountofCones = amountofCones - Conesbought
+//     console.log(Conesbought);
+//     console.log(amountofCones);
+//     if (amountofCones <= 1) {
+//         alert("We have 1 left for sale");
+//     } else {
+//         alert("We are all out");
+//     }
+//     } while (amountofCones > 0);
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    var conesBeingBought = Math.floor(Math.random() * 5) + 1;
+    if (conesBeingBought > allCones) {
+    console.log("Cannot sell you " + conesBeingBought + " I only have " + allCones);
+    } else  {
+    console.log(conesBeingBought + " cones sold...");
+    allCones = allCones - conesBeingBought;
+    }
+} while (allCones > 0);
+console.log("Yay! I sold them all!");
+
+
+
+
+
+
+
+
+
 
