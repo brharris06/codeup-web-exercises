bf73beb59f56d8d17f7d42d683c66f303a004824
Brandon Harris <brandon.r.harris06@gmail.com>
2022-02-07 13:45:05 -0600

    Adding Loop Lecture Morning Exercise

---
+++ js/loops-lecture.js
@@ -0,0 +1,31 @@
+// var slices = 4;
+// while(slices > 0) {
+//     console.log("I'll have a slice!");
+//     slices = slices - 1;
+//     console.log("Now there's " + slices + " left");
+// }
+// console.log("Awwwwwww........ no more pizza :( ");
+
+
+// var counter = 0;
+// while(counter <= 100){
+//     console.log(counter);
+//     // counter = counter + 1;
+//     counter ++;
+// }
+
+// var counter = 100;
+// while(counter >= 0){
+//     console.log(counter);
+//     counter --;
+// }
+
+var counter = 1;
+var number = 5;
+while (counter <=5){
+    number = number * 2;
+    console.log(number);
+    counter = counter + 1;
+}
+
+
