6704ac428b8aaaacefaa73f7edec2e06088e7fc8
Brandon Harris <brandon.r.harris06@gmail.com>
2022-02-11 09:34:00 -0600

    Adding JS-Warmup Activity

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -30,11 +30,62 @@
 //     console.log(price);
 // });
 
-var prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+// var prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+//
+// prices.forEach(function (price,index){
+//     console.log("Item number " + (index + 1) + " costs $" +price);
+// }
 
-prices.forEach(function (price,index){
-    console.log("Item number " + (index + 1) + " costs $" +price);
-});
+// groceryList.push("potatoes");
+// console.log(groceryList);
+// groceryList.pop();
+// console.log(groceryList);
+//
+// groceryList.unshift("sliced cheddar");
+// console.log(groceryList);
+// groceryList.shift();
+// console.log(groceryList);
+//
+// console.log(groceryList.indexOf("butter"));
+//
+// var indexOfButter = groceryList.indexOf("butter");
+// console.log("The butter is at index " + indexOfButter + ".");
+//
+// groceryList[2] = "peanut butter";
+// console.log(groceryList);
+// groceryList[indexOfButter] = "butter";
+// console.log(groceryList);
+
+// var firstHalfofList = groceryList.slice(0, 2);
+// console.log(firstHalfofList);
+// console.log(groceryList);
+// var secondHalfofList = groceryList.slice(2);
+// console.log(secondHalfofList);
+// firstHalfofList.push("peanut butter");
+// console.log(firstHalfofList);
+// groceryList = firstHalfofList.concat(secondHalfofList);
+// console.log(groceryList);
+//
+// groceryList.reverse();
+// console.log(groceryList);
+// groceryList.sort();
+// console.log(groceryList);
+
+
+var hamletQuote = "To be or not to be, that is the question";
+var noCommasHamletQuote = hamletQuote.replace(",", "").toLowerCase();
+var hamletQuoteArray = noCommasHamletQuote.split(" ");
+// console.log(hamletQuoteArray);
+//
+// console.log(hamletQuoteArray);
+// hamletQuoteArray.reverse();
+// console.log(hamletQuoteArray);
+
+var hamletQuoteBackwards = hamletQuoteArray.join(" ");
+console.log(hamletQuoteBackwards);
+var hamletQuoteWordList = hamletQuoteArray.join(",");
+console.log(hamletQuoteWordList);
+
 
 
 
--- js/js-warmups.js
+++ js/js-warmups.js
@@ -1,76 +1,103 @@
-"use strict";
+// "use strict";
+//
+// //Write a function that takes in an input and returns the string "I have no idea what you just said";
+//
+// function stringExample(string){
+//     return "I have no idea what you just said";
+// }
+//
+// console.log(stringExample("string"));
+//
+//
+// //write a function that takes in a number and returns that number times 2
+// //so if you give it 2 it returns 4
+// //if you give it 100 it returns 200
+//
+// function numberExample(x){
+//     return x * 2
+// }
+//
+// console.log(numberExample(5));
+//
+// // write a function that in a number and returns that number plus itself
+//
+// function morningExample(value){
+//     return value + value;
+// }
+//
+// console.log(morningExample(2));
+//
+// //now write a function that takes in two numbers, doubles each number, and then returns the sum of both numbers doubled.
+//
+//
+// function morningExample2(num1, num2){
+//     return (num1 * 2) + (num2 * 2);
+// }
+//
+// console.log(morningExample2(4,5));
+//
+// // write a function that takes in a number and squares it (that is, returns that number multiplied by itself
+//
+// function morningExample3(num1){
+//     return num1 * num1
+// }
+//
+// console.log(morningExample3(5));
+//
+//
+// //Write a function that takes in a string and returns its length
+//
+// function stringLength(string){
+//     return string.length;
+// }
+//
+// console.log(stringLength("How long is this string"));
+//
+// //write a function that takes in two strings and returns the sum of their lengths
+//
+// function sumOfStringLengths(string1, string2){
+//     return string1.length + string2.length;
+// }
+//
+// console.log(sumOfStringLengths("Hi", "There"));
+//
+// //Write a function that takes in a number and returns the string "even" or "odd" depending on whether the number is even or odd
+//
+// function oddOrEven(number){
+//     if (number % 2 === 0){
+//         return "even";
+//     } else {
+//         return "odd";
+//     }
+// }
+//
+// console.log(oddOrEven(4));
 
-//Write a function that takes in an input and returns the string "I have no idea what you just said";
 
-function stringExample(string){
-    return "I have no idea what you just said";
+// Write a function called removeBs that takes in an array of strings and returns an array of strings with all b’s removed from each string. Assume
+// the array contains only string elements.
+
+    // Examples...
+// removeBs([‘abc’, ‘ab’, ‘cat’, ‘Bay’]) // returns [‘ac’, ‘a’, ‘cat’, ‘ay’]
+
+
+function removeBsFromString(str) {
+    return str.replaceAll("b", "").replaceAll("B", "");
 }
 
-console.log(stringExample("string"));
-
-
-//write a function that takes in a number and returns that number times 2
-//so if you give it 2 it returns 4
-//if you give it 100 it returns 200
-
-function numberExample(x){
-    return x * 2
+function removeBs(arr) {
+    var output = [];
+    arr.forEach(function (str) {
+        output.push(str);
+    });
+    return output;
 }
 
-console.log(numberExample(5));
-
-// write a function that in a number and returns that number plus itself
-
-function morningExample(value){
-    return value + value;
-}
-
-console.log(morningExample(2));
-
-//now write a function that takes in two numbers, doubles each number, and then returns the sum of both numbers doubled.
+console.log(removeBs([‘abc’, ‘ab’, ‘cat’, ‘Bay’]));
 
 
-function morningExample2(num1, num2){
-    return (num1 * 2) + (num2 * 2);
-}
-
-console.log(morningExample2(4,5));
-
-// write a function that takes in a number and squares it (that is, returns that number multiplied by itself
-
-function morningExample3(num1){
-    return num1 * num1
-}
-
-console.log(morningExample3(5));
 
 
-//Write a function that takes in a string and returns its length
 
-function stringLength(string){
-    return string.length;
-}
-
-console.log(stringLength("How long is this string"));
-
-//write a function that takes in two strings and returns the sum of their lengths
-
-function sumOfStringLengths(string1, string2){
-    return string1.length + string2.length;
-}
-
-console.log(sumOfStringLengths("Hi", "There"));
-
-//Write a function that takes in a number and returns the string "even" or "odd" depending on whether the number is even or odd
-
-function oddOrEven(number){
-    if (number % 2 === 0){
-        return "even";
-    } else {
-        return "odd";
-    }
-}
-
-console.log(oddOrEven(4));
 
 
